c393445561437049eb182b423e31fffcae3fdfca
Alex <cabyca@yandex.ru>
2024-03-11 20:39:28 +0300

    lite balancer

--- providers.tf
+++ providers.tf
@@ -32,6 +32,48 @@ resource "yandex_vpc_subnet" "subnet" {
   v4_cidr_blocks = ["192.168.192.0/24"]
 }
 
+# Балансировщик нагрузки
+
+# создание целевой группы
+
+resource "yandex_lb_target_group" "foo" {
+  name      = "my-target-group"
+  folder_id = var.yc_folder_id
+
+  target {
+    subnet_id = "${yandex_vpc_subnet.subnet.id}"
+    address   = "${yandex_compute_instance.vm.network_interface.0.ip_address}"
+  }
+
+  target {
+    subnet_id = "${yandex_vpc_subnet.subnet.id}"
+    address   = "${yandex_compute_instance.vm2.network_interface.0.ip_address}"
+  }
+}
+
+resource "yandex_lb_network_load_balancer" "balancer" {
+  name = "my-network-load-balancer"
+
+  listener {
+    name = "my-listener"
+    port = 80
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+
+  attached_target_group {
+    target_group_id = "${yandex_lb_target_group.foo.id}"
+
+    healthcheck {
+      name = "http"
+      http_options {
+        port = 80
+      }
+    }
+  }
+}
+
 data "yandex_compute_image" "img" {
   family = "container-optimized-image"
 }
